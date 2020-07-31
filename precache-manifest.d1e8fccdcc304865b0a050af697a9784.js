9e08ef5ccd3648db4de11f9c52baa6affd77c465
Sourabh <hande.sourabh@gmail.com>
2020-07-31 19:16:19 +0530

    Updates

---
+++ precache-manifest.d1e8fccdcc304865b0a050af697a9784.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4322623d5ac8b996743f8a1844f9e7a0",
+    "url": "/Weather-tracker/index.html"
+  },
+  {
+    "revision": "200e2dfb554374aa03e0",
+    "url": "/Weather-tracker/static/css/main.aacc8e88.chunk.css"
+  },
+  {
+    "revision": "cc1a4b3c6df07659b1c0",
+    "url": "/Weather-tracker/static/js/2.b91fc6a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-tracker/static/js/2.b91fc6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200e2dfb554374aa03e0",
+    "url": "/Weather-tracker/static/js/main.82fc2605.chunk.js"
+  },
+  {
+    "revision": "3b927c9bc5b24974ab4c",
+    "url": "/Weather-tracker/static/js/runtime-main.1b33b096.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Weather-tracker/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Weather-tracker/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
